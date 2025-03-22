7e87bd267f951ed77d9d50e093f40a3214b27812
sipesdev <michael@sipes.dev>
2025-03-22 00:49:25 -0700

    Updated firefox.nix to actually work with extensions

--- flake.nix
+++ flake.nix
@@ -9,8 +9,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur = {
-      url = "github:nix-community/NUR";
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -20,47 +20,45 @@
       OverridePostUpdatePage = "";
       DontCheckDefaultBrowser = true;
       DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
-      DisplayMenuBar =
-        "default-off"; # alternatives: "always", "never" or "default-on"
+      DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
       SearchBar = "unified"; # alternative: "separate"
     };
-    profiles = {
-      default = {
-        id = 0;
-        name = "default";
-        isDefault = true;
-        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-          privacy-badger
-          decentraleyes
-          bitwarden
-        ];
-        settings = {
-          # Privacy
-          "extensions.pocket.enabled" = false;
-          "browser.newtabpage.pinned" = "";
-          "browser.topsites.contile.enabled" = false;
-          "browser.newtabpage.activity-stream.showSponsored" = false;
-          "browser.newtabpage.activity-stream.system.showSponsored" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    profiles.default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+        bitwarden
+        ublock-origin
+        privacy-badger
+        decentraleyes
+        return-youtube-dislike
+      ];
+      settings = {
+        # Privacy
+        "extensions.pocket.enabled" = false;
+        "browser.newtabpage.pinned" = "";
+        "browser.topsites.contile.enabled" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
 
-          # Search
-          "browser.search.defaultenginename" = "DuckDuckGo";
-          "browser.search.order.1" = "DuckDuckGo";
+        # Search
+        "browser.search.defaultenginename" = "DuckDuckGo";
+        "browser.search.order.1" = "DuckDuckGo";
 
-          # Basics
-          "signon.rememberSignons" = false;
-          "widget.use-xdg-desktop-portal.file-picker" = 1;
-          "browser.aboutConfig.showWarning" = false;
-          "browser.compactmode.show" = true;
-          "browser.cache.disk.enable" = false; # Be kind to hard drive
-          "extensions.autoDisableScopes" = 0;
+        # Basics
+        "signon.rememberSignons" = false;
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
+        "browser.aboutConfig.showWarning" = false;
+        "browser.compactmode.show" = true;
+        "browser.cache.disk.enable" = false; # Be kind to hard drive
+        "extensions.autoDisableScopes" = 0;
 
-          # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
-          # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
-          # then have Firefox open on some other workspace.
-          "widget.disable-workspace-management" = true;
-        };
+        # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+        # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+        # then have Firefox open on some other workspace.
+        "widget.disable-workspace-management" = true;
       };
     };
   };
