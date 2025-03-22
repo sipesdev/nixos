ec16d0b8993aa0234831677b62f7cbd9f498b382
sipesdev <michael@sipes.dev>
2025-03-21 22:44:02 -0700

    firefox declared

---
+++ modules/home-manager/firefox.nix
@@ -0,0 +1,64 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = true;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+        SearchBar = "unified"; # alternative: "separate"
+    };
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      privacy-badger
+      decentraleyes
+      bitwarden
+    ];
+    profile = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+      settings = {
+            # Privacy
+            "extensions.pocket.enabled" = false;
+            "browser.newtabpage.pinned" = "";
+            "browser.topsites.contile.enabled" = false;
+            "browser.newtabpage.activity-stream.showSponsored" = false;
+            "browser.newtabpage.activity-stream.system.showSponsored" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+
+            # Search
+            "browser.search.defaultenginename" = "DuckDuckGo";
+            "browser.search.order.1" = "DuckDuckGo";
+
+            # Basics
+            "signon.rememberSignons" = false;
+            "widget.use-xdg-desktop-portal.file-picker" = 1;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.compactmode.show" = true;
+            "browser.cache.disk.enable" = false; # Be kind to hard drive
+            "extensions.autoDisableScopes" = 0;
+
+            # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+            # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+            # then have Firefox open on some other workspace.
+            "widget.disable-workspace-management" = true;
+      };
+    };
+  };
+};
