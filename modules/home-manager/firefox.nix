6b896532c25205f5845639ecb79da29938ac6f25
sipesdev <michael@sipes.dev>
2025-03-21 22:53:35 -0700

    Firefox formatting

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -5,23 +5,24 @@
     enable = true;
     policies = {
       DisableTelemetry = true;
-        DisableFirefoxStudies = true;
-        EnableTrackingProtection = {
-          Value = true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-        };
-        DisablePocket = true;
-        DisableFirefoxAccounts = true;
-        DisableAccounts = true;
-        DisableFirefoxScreenshots = true;
-        OverrideFirstRunPage = "";
-        OverridePostUpdatePage = "";
-        DontCheckDefaultBrowser = true;
-        DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
-        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
-        SearchBar = "unified"; # alternative: "separate"
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+      DisplayMenuBar =
+        "default-off"; # alternatives: "always", "never" or "default-on"
+      SearchBar = "unified"; # alternative: "separate"
     };
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
       ublock-origin
@@ -29,35 +30,37 @@
       decentraleyes
       bitwarden
     ];
-    profile = {
-      id = 0;
-      name = "default";
-      isDefault = true;
-      settings = {
-            # Privacy
-            "extensions.pocket.enabled" = false;
-            "browser.newtabpage.pinned" = "";
-            "browser.topsites.contile.enabled" = false;
-            "browser.newtabpage.activity-stream.showSponsored" = false;
-            "browser.newtabpage.activity-stream.system.showSponsored" = false;
-            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        settings = {
+          # Privacy
+          "extensions.pocket.enabled" = false;
+          "browser.newtabpage.pinned" = "";
+          "browser.topsites.contile.enabled" = false;
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "browser.newtabpage.activity-stream.system.showSponsored" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
 
-            # Search
-            "browser.search.defaultenginename" = "DuckDuckGo";
-            "browser.search.order.1" = "DuckDuckGo";
+          # Search
+          "browser.search.defaultenginename" = "DuckDuckGo";
+          "browser.search.order.1" = "DuckDuckGo";
 
-            # Basics
-            "signon.rememberSignons" = false;
-            "widget.use-xdg-desktop-portal.file-picker" = 1;
-            "browser.aboutConfig.showWarning" = false;
-            "browser.compactmode.show" = true;
-            "browser.cache.disk.enable" = false; # Be kind to hard drive
-            "extensions.autoDisableScopes" = 0;
+          # Basics
+          "signon.rememberSignons" = false;
+          "widget.use-xdg-desktop-portal.file-picker" = 1;
+          "browser.aboutConfig.showWarning" = false;
+          "browser.compactmode.show" = true;
+          "browser.cache.disk.enable" = false; # Be kind to hard drive
+          "extensions.autoDisableScopes" = 0;
 
-            # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
-            # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
-            # then have Firefox open on some other workspace.
-            "widget.disable-workspace-management" = true;
+          # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+          # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+          # then have Firefox open on some other workspace.
+          "widget.disable-workspace-management" = true;
+        };
       };
     };
   };
