ba48c63f38751d8d9df8cccf3d00d04a61c04016
sipesdev <michael@sipes.dev>
2025-04-20 13:37:36 -0700

    Added global thunderbird config

--- modules/home-manager/thunderbird.nix
+++ modules/home-manager/thunderbird.nix
@@ -9,13 +9,11 @@
   };
 
   home.file = {
-    # Reverse symlink to update home files in my git directory to sync
-    # ".thunderbird/profiles.ini" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repos/dotfiles/.thunderbird/profiles.ini";
-    # };
-    # ".thunderbird/michael" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}repos/dotfiles/.thunderbird/";
-    #   recursive = true;
-    # };
+    ".thunderbird/profiles.ini" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repos/dotfiles/.thunderbird/profiles.ini";
+    };
+    ".thunderbird/michael/prefs.js" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repos/dotfiles/.thunderbird/michael/prefs.js";
+    };
   };
 }
