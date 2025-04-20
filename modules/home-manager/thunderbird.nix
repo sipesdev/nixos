2c57ce9a6b5f0051f99ceb65d734dfed13a11256
sipesdev <michael@sipes.dev>
2025-04-20 00:04:35 -0700

    Added thunderbird to sync with dotfiles

--- modules/home-manager/thunderbird.nix
+++ modules/home-manager/thunderbird.nix
@@ -7,4 +7,15 @@
       isDefault = true;
     };
   };
+
+  home.file = {
+    # Reverse symlink to update home files in my git directory to sync
+    "repos/dotfiles/.thunderbird/profiles.ini" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.thunderbird/profiles.ini";
+    };
+    "repos/dotfiles/.thunderbird/michael" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.thunderbird/";
+      recursive = true;
+    };
+  };
 }
