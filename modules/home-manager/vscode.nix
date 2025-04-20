73f65dc1917eb70452045dca671ba4a79a4ff381
sipesdev <michael@sipes.dev>
2025-04-19 23:41:28 -0700

    Reverse symlink with dotfiles directory for easier management

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -4,38 +4,15 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.fhs;
-    profiles.default = {
-      extensions = with pkgs.vscode-extensions; [
-        ms-dotnettools.csharp
-        ms-dotnettools.csdevkit
-        ms-vscode.cpptools
-        ms-vscode.cmake-tools
-        ms-python.python
-        ms-python.debugpy
-        redhat.vscode-xml
-        redhat.vscode-yaml
-        redhat.java
-        rust-lang.rust-analyzer
-        tamasfe.even-better-toml
-        jnoortheen.nix-ide
-        arrterian.nix-env-selector
-        catppuccin.catppuccin-vsc
-        catppuccin.catppuccin-vsc-icons
-      ];
-      userSettings = {
-        "password-store" = "gnome";
-        "update.mode" = "none";
+  };
 
-        # Theming
-        "window.titleBarStyle" = "native";
-        "workbench.colorTheme" = "Catppuccin Mocha";
-        "workbench.preferredDarkColorTheme" = "Catppuccin Mocha";
-        "workbench.iconTheme" = "catppuccin-mocha";
-
-        # Personal settings
-        "editor.minimap.enabled" = false;
-        "editor.stickyScroll.enabled" = false;
-      };
+  home.file = {
+    # Reverse symlink to update home files in my git directory to sync
+    "repos/dotfiles/Code/User/settings.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Code/User/settings.json";
+    };
+    "repos/dotfiles/.vscode/extensions/extensions.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.vscode/extensions/extensions.json";
     };
   };
 
